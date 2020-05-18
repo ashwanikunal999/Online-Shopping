2c52bbb24ebb70226d0ec1972137372c52e15336
Ashwani Kunal Singh <ashwanikunal999@gmail.com>
2020-05-18 11:44:30 +0530

    Complete the all module this project.

---
+++ shoppingbackend/src/main/java/com/aks/shoppingbackend/dao/UserDAO.java
@@ -0,0 +1,26 @@
+package com.aks.shoppingbackend.dao;
+
+
+import java.util.List;
+
+import com.aks.shoppingbackend.dto.Address;
+import com.aks.shoppingbackend.dto.User;
+
+public interface UserDAO {
+
+	// user related operation
+	User getByEmail(String email);
+	User get(int id);
+
+	boolean add(User user);
+	
+	// adding and updating a new address
+	Address getAddress(int addressId);
+	boolean addAddress(Address address);
+	boolean updateAddress(Address address);
+	Address getBillingAddress(int userId);
+	List<Address> listShippingAddresses(int userId);
+	
+
+	
+}
